c15517903393ef5089292a25fd0023fb7e1c5fcb
SimonKonar <simon.konar@gmail.com>
2024-07-17 10:20:57 +0200

    feat: add sample donor tests

--- MIABIS_on_FHIR/MoF_sample_donor.py
+++ MIABIS_on_FHIR/MoF_sample_donor.py
@@ -9,11 +9,11 @@ from fhirclient.models.meta import Meta
 from fhirclient.models.patient import Patient
 
 from MIABIS_on_FHIR.gender import Gender
-
+from _constants import DONOR_DATASET_TYPE
 
 class MoFSampleDonor:
     """Class representing a sample donor/patient as defined by the MIABIS on FHIR profile."""
-    def __init__(self, identifier: str, gender: Gender = None, birth_date: datetime = None, datasetType: str = None):
+    def __init__(self, identifier: str, gender: Gender = None, birth_date: datetime = None, dataset_type: str = None):
         """
         :param identifier: Sample donor identifier
         :param gender: Gender of the donor
@@ -23,9 +23,15 @@ class MoFSampleDonor:
         if not isinstance(identifier, str):
             raise TypeError("Identifier must be string")
         self._identifier = identifier
+        if gender is not None and not isinstance(gender, Gender):
+            raise TypeError("Gender must be from a list of values: " + str(Gender.list()))
         self._gender = gender
+        if birth_date is not None and not isinstance(birth_date, datetime):
+            raise TypeError("Date of birth must be a datetime.")
         self._date_of_birth = birth_date
-        self._datasetType = datasetType
+        if dataset_type is not None and dataset_type not in DONOR_DATASET_TYPE:
+            raise ValueError(f"bad dataset type: has to be one of the following: {DONOR_DATASET_TYPE}")
+        self._dataset_type = dataset_type
 
     @property
     def identifier(self) -> str:
@@ -57,15 +63,19 @@ class MoFSampleDonor:
 
     @date_of_birth.setter
     def date_of_birth(self, birth_date: datetime):
+        if not isinstance(birth_date, datetime):
+            raise TypeError("Date of birth must be a datetime.")
         self._date_of_birth = birth_date
 
     @property
-    def datasetType(self) -> str:
-        return self._datasetType
+    def dataset_type(self) -> str:
+        return self._dataset_type
 
-    @datasetType.setter
-    def datasetType(self, datasetType: str):
-        self._datasetType = datasetType
+    @dataset_type.setter
+    def dataset_type(self, dataset_type: str):
+        if dataset_type not in DONOR_DATASET_TYPE:
+            raise ValueError(f"bad dataset type: has to be one of the following: {DONOR_DATASET_TYPE}")
+        self._dataset_type = dataset_type
 
     def to_fhir(self) -> Patient:
         """Return sample donor representation in FHIR"""
@@ -79,7 +89,7 @@ class MoFSampleDonor:
         if self.date_of_birth is not None:
             fhir_patient.birthDate = FHIRDate()
             fhir_patient.birthDate.date = self.date_of_birth.date()
-        if self.datasetType is not None:
+        if self.dataset_type is not None:
             extensions.append(self.__create_dataset_extension())
         if extensions:
             fhir_patient.extension = extensions
@@ -96,5 +106,5 @@ class MoFSampleDonor:
         fhir_dataset.url = "https://example.org/StructureDefinition/datasetType"
         fhir_dataset.valueCodeableConcept = CodeableConcept()
         fhir_dataset.valueCodeableConcept.coding = [Coding()]
-        fhir_dataset.valueCodeableConcept.coding[0].code = self.datasetType
+        fhir_dataset.valueCodeableConcept.coding[0].code = self.dataset_type
         return fhir_dataset
