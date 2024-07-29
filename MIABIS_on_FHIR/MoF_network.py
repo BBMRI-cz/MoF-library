36bf2d0fb8ba77c72a6831ce1a9b0784a994cdea
SimonKonar <simon.konar@gmail.com>
2024-07-29 08:43:51 +0200

    feat: add network tests

--- MIABIS_on_FHIR/MoF_network.py
+++ MIABIS_on_FHIR/MoF_network.py
@@ -1,16 +1,24 @@
+from fhirclient.models.codeableconcept import CodeableConcept
+from fhirclient.models.coding import Coding
 from fhirclient.models.extension import Extension
 from fhirclient.models.group import Group
 from fhirclient.models.identifier import Identifier
 from _constants import NETWORK_COMMON_COLLAB_TOPICS
 
 
-class Network():
+class MoFNetwork():
     """Class representing a group of interconnected biobanks or collections with defined common governance"""
 
     def __init__(self, identifier: str, name: str, common_collaboration_topics: list[str] = None):
+        if not isinstance(identifier, str):
+            raise TypeError("Identifier must be string")
+        if not isinstance(name, str):
+            raise TypeError("Name must be string")
         self._identifier = identifier
         self._name = name
         if common_collaboration_topics is not None:
+            if not isinstance(common_collaboration_topics, list):
+                raise TypeError("Common collaboration topics must be a list")
             for topic in common_collaboration_topics:
                 if topic not in NETWORK_COMMON_COLLAB_TOPICS:
                     raise ValueError(f"{topic} is not a valid code for common collaboration topics")
@@ -22,6 +30,8 @@ class Network():
 
     @identifier.setter
     def identifier(self, identifier: str):
+        if not isinstance(identifier, str):
+            raise TypeError("Identifier must be string")
         self._identifier = identifier
 
     @property
@@ -30,6 +40,8 @@ class Network():
 
     @name.setter
     def name(self, name: str):
+        if not isinstance(name, str):
+            raise TypeError("Name must be string")
         self._name = name
 
     @property
@@ -37,7 +49,12 @@ class Network():
         return self._common_collaboration_topics
 
     @common_collaboration_topics.setter
-    def common_collaboration_topics(self, common_collaboration_topics: str):
+    def common_collaboration_topics(self, common_collaboration_topics: list[str]):
+        if not isinstance(common_collaboration_topics, list):
+            raise TypeError("Common collaboration topics must be a list")
+        for topic in common_collaboration_topics:
+            if topic not in NETWORK_COMMON_COLLAB_TOPICS:
+                raise ValueError(f"{topic} is not a valid code for common collaboration")
         self._common_collaboration_topics = common_collaboration_topics
 
     def to_fhir(self) -> Group:
@@ -46,7 +63,6 @@ class Network():
         network.name = self._name
         network.active = True
         network.type = "person"
-        network.type = "network"
         if self._common_collaboration_topics is not None:
             extensions = []
             for topic in self._common_collaboration_topics:
@@ -63,5 +79,8 @@ class Network():
     def __create_extension(self, url: str, value: str):
         extension = Extension()
         extension.url = url
-        extension.valueString = value
+        extension.valueCodeableConcept = CodeableConcept()
+        extension.valueCodeableConcept.coding = [Coding()]
+        extension.valueCodeableConcept.coding[0].code = value
+        extension.valueCodeableConcept.coding[0].system = "http://example.com/common-collaboration-topics-vs"
         return extension
