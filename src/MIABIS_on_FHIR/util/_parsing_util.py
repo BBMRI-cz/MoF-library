befe8921162bc4c42d151eb42231cc02daced57e
SimonKonar <simon.konar@gmail.com>
2024-10-14 11:32:32 +0200

    feat:add parse date from string function

--- src/MIABIS_on_FHIR/util/_parsing_util.py
+++ src/MIABIS_on_FHIR/util/_parsing_util.py
@@ -1,3 +1,8 @@
+from dateutil import parser as date_parser
+from datetime import datetime
+from datetime import date
+
+
 def get_nested_value(data: dict, keys: list):
     for key in keys:
         if isinstance(data, list) and isinstance(key, int):
@@ -13,6 +18,7 @@ def parse_reference_id(reference: str) -> str:
     """Helper method to parse reference id."""
     return reference.split("/")[-1]
 
+
 def parse_contact(contact: dict) -> dict:
     """Helper method to parse contact information."""
     return {
@@ -20,3 +26,8 @@ def parse_contact(contact: dict) -> dict:
         "surname": contact.get("name", {}).get("family", ""),
         "email": contact.get("telecom", [{}])[0].get("value", "")
     }
+
+
+def parse_date_from_string(date_str: str) -> date:
+    """Parse date from string."""
+    return date_parser.parse(date_str).date()
